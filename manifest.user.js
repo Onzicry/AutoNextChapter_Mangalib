63e7a51a2fb0b38b23eb4b6a9ab53ec6d168b95c
Onzicry <deltaradune@yandex.ru>
2025-05-02 18:39:40 +0300

    ..

---
+++ manifest.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @namespace       https://github.com/qt-kaneko/Shikiplayer
+// @name            $(NAME)
+// @description     $(DESCRIPTION)
+// @description:ru  $(DESCRIPTION_RU)
+// @author          $(AUTHOR)
+// @homepageURL     $(HOMEPAGE_URL)
+// @copyright       2025, Onzicry
+// @license         AGPL-3.0-or-later; https://www.gnu.org/licenses/agpl-3.0.txt
+// @version         $(VERSION)
+//
+// @updateURL       manifest.user.js
+// @downloadURL     manifest.user.js
+//
+// @match           
+// @match           
+// @match           
+//
+// @run-at          document-end
+// ==/UserScript==
+
+$(USERSCRIPT)
