88fb0e888e36c79324b5c90c90a650707bad4aaf
Onzicry <deltaradune@yandex.ru>
2025-05-02 19:46:05 +0300

    .

---
+++ assets/install.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Mangalib Auto Next Chapter (Mobile Support)
+// @namespace    https://github.com/Onzicry/AutoNextChapter_Mangalib
+// @version      1.6
+// @description  Автоматический переход на следующую главу (ПК + мобильная версия)
+// @author       Onzicry
+// @match        https://mangalib.me/*/read*
+// @match        https://mangalib.me/*
+// @match        https://hentailib.me/*/read*
+// @match        https://hentailib.me/*
+// @match        https://slashlib.me/*/read*
+// @match        https://slashlib.me/*
+// @grant        none
+// @license      AGPL-3.0-or-later; https://www.gnu.org/licenses/agpl-3.0.txt
+// @downloadURL  https://raw.githubusercontent.com/ваш-логин/ваш-репозиторий/ветка/имя-файла.user.js
+// @updateURL    https://raw.githubusercontent.com/ваш-логин/ваш-репозиторий/ветка/имя-файла.user.js
+// ==/UserScript==
